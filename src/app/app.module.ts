bc090384be64e711335ee066afcc0425b1cdc611
Satendra Kumar <satendramca2016@gmail.com>
2022-06-22 22:21:44 +0530

    initial code for angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
+import { MainComponent } from './MyComponents/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
